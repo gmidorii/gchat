f62856e7f48c89cb5308b2edb9c8e881a53e17b1
tagasouichiro <stitow14@gmail.com>
2017-11-24 08:01:54 +0900

    Update hub test

--- hub_test.go
+++ hub_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
 
 func TestExtractRoom(t *testing.T) {
 	cases := []struct {
@@ -13,16 +17,29 @@ func TestExtractRoom(t *testing.T) {
 		{[]Roomer{&Room{name: "hoge"}}, "new", &Room{name: "new"}},
 	}
 
+	// set up
+	root, err := os.Getwd()
+	if err != nil {
+		t.Errorf("failed pwd: %v", err)
+	}
 	for _, c := range cases {
 		hub := Hub{
-			Rooms: c.rooms,
+			Rooms:       c.rooms,
+			historyRoot: root,
 		}
+		// execute
 		r, err := hub.ExtractRoom(c.in)
 		if err != nil {
 			t.Errorf("failed extract: %v", err)
 		}
 		if c.want.NameStr() != r.NameStr() {
 			t.Errorf("not expected room\n e:%s \n a:%s", c.want.NameStr(), r.NameStr())
+		}
+
+		// tear down
+		err = os.Remove(filepath.Join(hub.historyRoot, r.NameStr()))
+		if err != nil {
+			t.Errorf("failed rm history file: %v", err)
 		}
 	}
 }
